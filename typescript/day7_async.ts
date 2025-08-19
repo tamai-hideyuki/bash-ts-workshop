610c6fd05641ea1693a0388b1b90a254dc6abe57
tamai-hideyuki <hide27yuki@gmail.com>
2025-08-20 06:07:18 +0900

    day7

---
+++ typescript/day7_async.ts
@@ -0,0 +1,49 @@
+//fetchでAPIを取得
+
+async function fetchData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    console.log(data);
+}
+
+//try catchでエラー処理
+async function errorHandling() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+        if (!response.ok) {
+            throw new Error('Bad Request: ${response.status}');
+        }
+        const data = await response.json();
+        console.log('SUCCESS:', data);
+    } catch (error) {
+        console.error('ERROR:', error);
+    }
+}
+
+errorHandling();
+
+
+
+//Promise.all で並列実行
+async function promiseFetch() {
+    try {
+        const urls = [
+            "https://jsonplaceholder.typicode.com/posts/1",
+            "https://jsonplaceholder.typicode.com/posts/2",
+            "https://jsonplaceholder.typicode.com/posts/3",
+        ];
+        const response = await Promise.all(
+            urls.map((url) => fetch(url))
+        );
+
+        const data = await Promise.all(
+            response.map((response) => response.json())
+        );
+
+        console.log("ALL SUCCESS:", data);
+    } catch (error) {
+        console.error("ERROR:", error);
+    }
+}
+
+promiseFetch();
